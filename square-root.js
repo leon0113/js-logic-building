be37abb735ca7f747cf7a8bfdfb8926be1fce04a
Leon <tahjib.leon399@gmail.com>
2023-08-28 22:55:29 +0600

    Square Root

---
+++ square-root.js
@@ -0,0 +1,11 @@
+// const a = 25;
+// const result = Math.sqrt(a);
+// console.log(result);
+
+function findSqrt(a) {
+    const result = Math.sqrt(a);
+    return result;
+}
+
+const output = findSqrt(64);
+console.log(output);
