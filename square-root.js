52c6386d808f9526eaee541018db8eaf612cf13a
Leon <tahjib.leon399@gmail.com>
2023-08-28 23:06:48 +0600

    dynamic square-root

--- square-root.js
+++ square-root.js
@@ -1,11 +1,27 @@
-// const a = 25;
-// const result = Math.sqrt(a);
-// console.log(result);
 
-function findSqrt(a) {
-    const result = Math.sqrt(a);
-    return result;
-}
 
-const output = findSqrt(64);
-console.log(output);
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Please enter a number: ', (input) => {
+    const num = parseFloat(input); // Convert the input to a number
+    if (!isNaN(num)) { // Check if the conversion was successful
+        function findSqrt(x) {
+            const result = Math.sqrt(x);
+            return result;
+        }
+        console.log(`Square root of ${num} is: ${findSqrt(num)}`);
+    } else {
+        console.log('Invalid input. Please enter a valid number.');
+    }
+
+    rl.close();
+});
+
+
+
+
