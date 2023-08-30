962b8147bc65adea1565b9c1479d45bf45624869
Leon <tahjib.leon399@gmail.com>
2023-08-30 18:31:28 +0600

    test

---
+++ test.js
@@ -0,0 +1,8 @@
+// 90 deg tringle area
+
+function tringle1(base, height) {
+    return (height * base) / 2;
+}
+
+console.log(tringle1(10, 5));
+
---
+++ tringle-area.js
@@ -0,0 +1,9 @@
+// 90 deg tringle area
+
+function tringle1(base, height) {
+    return (height * base) / 2;
+}
+
+console.log(tringle1(10, 5));
+
+
